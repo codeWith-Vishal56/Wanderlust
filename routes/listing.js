beb98dfa3184bdfd310298a11a77a28168d04bdd
Vishal Maurya <codewithvishal56@gmail.com>
2026-09-06 10:51:03 +0530

    using router.route

--- routes/listing.js
+++ routes/listing.js
@@ -1,37 +1,37 @@
 const express = require("express");
 const router = express.Router();
-const {isloggedin , isOwner, validateListing} = require("../middleware");
+const { isloggedin, isOwner, validateListing } = require("../middleware");
 const listingController = require("../controllers/listing");
 
 // Express 5 automatically handles async errors.
 // No need to wrap async routes with wrapAsync.
 
-// SHOW ALL LISTING 
+// SHOW ALL LISTING AND CREATE NEW LISTING
 
-router.get("/", listingController.index);
-
+router
+  .route("/")
+  .get(listingController.index)
+  .post(isloggedin, validateListing, listingController.newListing);
 
 // ADD NEW LISTING
 
-router.get("/new",isloggedin, listingController.renderNewListingForm);
+router.get("/new", isloggedin, listingController.renderNewListingForm);
 
-router.post("/",isloggedin,validateListing, listingController.newListing)
+// SHOW LISTING DETAILS & LISTING DETAILS EDIT & LISTING DELETE
 
-// SHOW LISTING DETAILS
+router
+  .route("/:id")
+  .get(listingController.showListing)
+  .put(isloggedin, isOwner, validateListing, listingController.editListing)
+  .delete(isloggedin, isOwner, listingController.destroyListing);
 
-router.get("/:id",listingController.showListing);
+// LISTING DETAILS EDIT FORM
 
-
-
-// LISTING DETAILS EDIT 
-
-router.get("/edit/:id",isloggedin,isOwner,listingController.renderEditListingForm);
-
-router.put("/:id",isloggedin,isOwner, validateListing , listingController.editListing);
-
-// LISTING DELETE
-
-router.delete("/:id", isloggedin,isOwner,listingController.destroyListing);
-
+router.get(
+  "/edit/:id",
+  isloggedin,
+  isOwner,
+  listingController.renderEditListingForm,
+);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,30 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const {saveRedirectUrl} = require("../middleware");
+const { saveRedirectUrl } = require("../middleware");
 const userController = require("../controllers/user");
+
 // signup
 
-router.get("/signup", userController.renderSignupForm);
-
-router.post("/signup", userController.signup);
+router
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(userController.signup);
 
 // login
 
-router.get("/login", userController.renderLoginForm);
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login,
+  );
 
-router.post(
-  "/login", saveRedirectUrl ,
-  passport.authenticate("local", {
-    failureRedirect: "/login",
-    failureFlash: true,
-  }),
-  userController.login,
-);
-
-router.get("/logout",userController.logout);
+router.get("/logout", userController.logout);
 
 module.exports = router;
